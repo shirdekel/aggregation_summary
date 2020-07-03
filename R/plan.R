6436bf7bf8f67abb41eb33f5a8bf32fff1629a84
Shir Dekel <shirdekel@yahoo.com.au>
2020-07-03 11:04:13 +1000

    Add plan

--- R/plan.R
+++ R/plan.R
@@ -1,10 +1,37 @@
 the_plan <-
   drake_plan(
+    data = get_data(),
+    full = apa_plot(data = data, 
+                    iv1 = "Awareness", 
+                    iv2 = "Alignment",
+                    iv3 = Presentation,
+                    iv1.lab = "Awareness of project set size",
+                    dv = "risk",
+                    dv.lab = "Mean proportions of project acceptance"),
+    awareness = apa_plot(data = data, 
+                         iv1 = "Awareness", 
+                         iv1.lab = "Awareness of project set size",
+                         dv = "risk",
+                         dv.lab = "Mean proportions of project acceptance"),
+    presentation = apa_plot(data = data, 
+                            iv1 = "Presentation", 
+                            iv1.lab = "Awareness of project set size",
+                            dv = "risk",
+                            dv.lab = "Mean proportions of project acceptance"),
+    similarity_presentation = apa_plot(data = data, 
+                                       iv1 = "Alignment",
+                                       iv1.lab = "Similarity",
+                                       iv2 = "Presentation", 
+                                       dv = "risk",
+                                       dv.lab = "Mean proportions of project acceptance"),
+    data_trials = clean_data(aggregation::dat),
+    plot_separate_trial_awareness = get_plot_separate_trial_awareness(data_trials),
     
-    target_name = target(
+    memo = target(
       command = {
+        file_in()
         rmarkdown::render(knitr_in("doc/aggregation_summary.Rmd"))
-        file_out("doc/aggregation_summary.html")
+        file_out("doc/aggregation_summary.pdf")
       }
     )
     
